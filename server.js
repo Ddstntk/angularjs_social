595428caf43be9aec9fb2dfa24fd7710feb2feb2
Ddstntk <konrabana@gmail.com>
2018-07-15 19:40:54 +0200

    arrangement

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var Post = require('./model/post')
+var Post = require('./models/post')
 
 
 var app = express()
 app.use(bodyParser.json())
 
-
-app.get('/', function (req, res) {
-  res.sendfile('layouts/posts.html')
-})
-
-app.get('/api/posts', function (req, res, nest) {
-  Post.find(function(err, posts) {
-    if (err) { return next(err) }
-    res.json(posts)
-  })
-})
-
-
-app.post('/api/posts', function (req, res) {
-  var post = new Post({
-    username: req.body.username,
-    body: req.body.body
-  })
-  post.save(function(err, post) {
-    if (err) { return next(err)}
-    res.json(201, post)
-  })
-})
-
+app.use('/api/posts', require('./controllers/api/posts'))
+app.use(require('./controllers/static'))
 
 app.listen(3000, function() {
   console.log('serwer nasluchuje 3000')
